070adf32190323c7b1c7084ed24a183f832e99e5
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-07-28 17:15:41 +0530

    100 years added

---
+++ Python/100 years.py
@@ -0,0 +1,8 @@
+'''Create a program that reads the length and width of a farmers field from the user in feet.
+Display the area of the field in acres.
+'''
+
+name = input()
+age = int(input())
+year = str((2017 - age)+100)
+print(name + " will be 100 years old in " + year)
